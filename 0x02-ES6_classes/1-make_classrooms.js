fe361b3c34be88f14c6cfe954064c9d6ae1f1b8c
Ken Wekesa <wanyamak884@gmail.com>
2024-06-30 21:17:12 +0300

    lint code

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom'
  * Implement a function named initializeRooms.
  * It should return an array of 3 ClassRoom
  * objects with the sizes 19, 20, and 34 (in this order). 
- * */
+ */
 
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
