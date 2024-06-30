0b1c05c045663e42e342e23f6f6126cdea682dde
Ken Wekesa <wanyamak884@gmail.com>
2024-06-30 21:15:46 +0300

    make classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,16 +1,10 @@
+import ClassRoom from './0-classroom'
 /**
- * Class representing a classroom.
- */
-export default class ClassRoom {
-  /**
-   * Create a classroom.
-   * @param {Number} maxStudentsSize - The maximum number of students allowed in the classroom.
-   */
-  constructor(maxStudentsSize) {
-    /**
-     * @type {Number}
-     * @private
-     */
-    this._maxStudentsSize = maxStudentsSize;
-  }
+ * Implement a function named initializeRooms.
+ * It should return an array of 3 ClassRoom
+ * objects with the sizes 19, 20, and 34 (in this order). 
+ * */
+
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
