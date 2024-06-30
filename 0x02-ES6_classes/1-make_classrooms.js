25be8052b2cd955407189b649e904783d0fe996e
Ken Wekesa <wanyamak884@gmail.com>
2024-06-30 23:45:55 +0300

    added semicolon, deleted space

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,10 @@
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
 /**
  * Implement a function named initializeRooms.
  * It should return an array of 3 ClassRoom
- * objects with the sizes 19, 20, and 34 (in this order). 
+ * objects with the sizes 19, 20, and 34 (in this order).
  */
-
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
