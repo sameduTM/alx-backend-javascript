c6789937dd89ebce96d792c6dcbc0df77a0ac097
Ken Wekesa <wanyamak884@gmail.com>
2024-07-03 00:18:56 +0300

    basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Returns an array of student objects.
+ * Each object contains the properties: id, firstName, and location.
+ * 
+ * @returns {Array<Object>} Array of student objects
+ */
+function getListStudents() {
+    return [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
+  }
