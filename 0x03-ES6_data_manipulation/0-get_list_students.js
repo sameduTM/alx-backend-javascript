85786a87d7ec483543311d186b372fb02d65f5fc
Ken Wekesa <wanyamak884@gmail.com>
2024-07-03 16:32:19 +0300

    eslint check

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,13 @@
 /**
  * Returns an array of student objects.
  * Each object contains the properties: id, firstName, and location.
- * 
+ *
  * @returns {Array<Object>} Array of student objects
  */
 export default function getListStudents() {
-    return [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
-  }
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
