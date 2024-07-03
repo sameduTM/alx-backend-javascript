8cb20b01cd9d01eaea237695939ff516fd1a3021
Ken Wekesa <wanyamak884@gmail.com>
2024-07-03 17:14:00 +0300

    set data structuer

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * Function to create a Set from an array.
+ *
+ * @param {Array} arr - The array of elements.
+ * @returns {Set} - A new Set containing the elements of the array.
+ */
+export default function setFromArray(arr) {
+  // Create a new Set from the array
+  return new Set(arr);
+}
