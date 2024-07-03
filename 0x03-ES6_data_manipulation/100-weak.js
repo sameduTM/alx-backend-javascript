b32851b8fb5dc3116df0c4d3ac7fd1e0652da260
Ken Wekesa <wanyamak884@gmail.com>
2024-07-03 17:59:21 +0300

    lint fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,19 +2,19 @@ export const weakMap = new WeakMap();
 
 /**
  * Function to track the number of times an endpoint is queried.
- * 
+ *
  * @param {Object} endpoint - The endpoint object with protocol and name properties.
  * @throws {Error} - Throws an error if the number of queries is >= 5 for an endpoint.
  */
 export function queryAPI(endpoint) {
-    if (!weakMap.has(endpoint)) {
-        weakMap.set(endpoint, 0);
-    }
-    
-    const queryCount = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, queryCount);
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
 
-    if (queryCount >= 5) {
-        throw new Error('Endpoint load is high');
-    }
+  const queryCount = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, queryCount);
+
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
