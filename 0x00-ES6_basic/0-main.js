eb8bd743932279e8c3c13132295099d6f3b0c090
wanyamak884@gmail.com <wanyamak884@gmail.com>
2025-02-20 12:50:47 +0300

    added README.md file

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,15 +1,3 @@
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}
+import { taskFirst, taskNext } from './0-constants.js';
 
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
+console.log(`${taskFirst()} ${taskNext()}`);
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,12 @@
+0x00. ES6 Basics
+JavaScript
+ES6
+Resources
+Read or watch:
+
+ECMAScript 6 - ECMAScript 2015
+Statements and declarations
+Arrow functions
+Default parameters
+Rest parameter
+Javascript ES6 — Iterables and Iterators
