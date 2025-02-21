e0a2c37056e0086b5dea6ce5d84d28bcc7acdad4
wanyamak884@gmail.com <wanyamak884@gmail.com>
2025-02-21 09:53:31 +0300

    For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array;
-  for (const idx of newArray) {
-    const value = newArray.indexOf(idx);
-    newArray[value] = appendString + idx;
+  // const newArray = array;
+  for (const idx of array) {
+    const value = array.indexOf(idx);
+    array[value] = appendString + idx;
   }
-  return newArray;
+  return array;
 }
