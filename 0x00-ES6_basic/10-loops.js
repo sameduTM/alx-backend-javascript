d30db2e4e93f32bbf7130f7b18d23abf2fa79879
wanyamak884@gmail.com <wanyamak884@gmail.com>
2025-02-21 10:09:49 +0300

    no params reassign

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // const newArray = array;
+  /* eslint no-param-reassign: "error" */
   for (const idx of array) {
     const value = array.indexOf(idx);
     array[value] = appendString + idx;
