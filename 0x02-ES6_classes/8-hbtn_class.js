17b5f52b840b29aa4e77af8b4efc49a070bc6b61
Ken Wekesa <wanyamak884@gmail.com>
2025-03-04 16:44:53 +0300

    Primitive value types

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,48 +1,16 @@
-/**
- * Class representing a Holberton class.
- */
-class HolbertonClass {
-  /**
-   * Create a Holberton class.
-   * @param {Number} size - The size of the class.
-   * @param {String} location - The location of the class.
-   */
+export default class ALXClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
-  /**
-   * Get the size of the class.
-   * @returns {Number} The size of the class.
-   */
-  get size() {
-    return this._size;
-  }
-
-  /**
-   * Get the location of the class.
-   * @returns {String} The location of the class.
-   */
-  get location() {
-    return this._location;
-  }
-
-  /**
-   * Cast the class into a Number, returning the size.
-   * @returns {Number} The size of the class.
-   */
-  valueOf() {
-    return this._size;
-  }
-
-  /**
-   * Cast the class into a String, returning the location.
-   * @returns {String} The location of the class.
-   */
-  toString() {
-    return this._location;
+  [Symbol.toPrimitive](hint) {
+    let returnValue = '';
+    if (hint === 'number') {
+      returnValue = this._size;
+    } if (hint === 'string') {
+      returnValue = this._location;
+    }
+    return returnValue;
   }
 }
-
-export default HolbertonClass;
