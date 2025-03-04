f6d2a6fff93398caae430645687527651f18d5da
Ken Wekesa <wanyamak884@gmail.com>
2025-03-04 14:52:49 +0300

    abstract classes and methods on JS

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,20 @@
 export default class Building {
-  constructor(sft) {
-    this.sft = sft;
+  constructor(sqft) {
     if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
+      if (typeof evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
       }
     }
+    this._sqft = sqft;
   }
 
-  get sft() {
-    return this._sft;
+  // Getter
+  get sqft() {
+    return this._sqft;
   }
 
-  set sft(value) {
-    this._sft = value;
+  // Setter
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
