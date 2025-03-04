1128ced8939532a4f3e5484169ab385005802de8
Ken Wekesa <wanyamak884@gmail.com>
2025-03-04 15:44:06 +0300

    Symbols - Metaprogramming

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,40 +1,10 @@
-/**
- * Class representing an airport.
- */
-class Airport {
-  /**
-   * Create an airport.
-   * @param {String} name - The name of the airport.
-   * @param {String} code - The code of the airport.
-   */
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  /**
-   * Get the name of the airport.
-   * @returns {String} The name of the airport.
-   */
-  get name() {
-    return this._name;
-  }
-
-  /**
-   * Get the code of the airport.
-   * @returns {String} The code of the airport.
-   */
-  get code() {
-    return this._code;
-  }
-
-  /**
-   * Return the default string description of the airport.
-   * @returns {String} The airport code.
-   */
-  toString() {
-    return this._code;
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
   }
 }
-
-export default Airport;
