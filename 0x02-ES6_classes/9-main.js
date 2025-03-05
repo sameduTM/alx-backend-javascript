5f39779b75c5efe2b79d166eb4c9413ba603f4cc
Ken Wekesa <wanyamak884@gmail.com>
2025-03-05 15:12:58 +0300

    push untracked files

--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -1,9 +1,4 @@
-import listOfStudents from "./9-hoisting.js";
+import listOfStudents, { StudentALX, ALXClass } from './9-hoisting.js';
 
-console.log(listOfStudents);
-
-const listPrinted = listOfStudents.map(
-    student => student.fullStudentDescription
-);
-
-console.log(listPrinted)
+console.log(listOfStudents[0] instanceof StudentALX);
+console.log(listOfStudents[0].alxClass instanceof ALXClass);
