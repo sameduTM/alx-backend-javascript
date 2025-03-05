ae034f648882933bac22cbf3c54343c230cb3bfa
Ken Wekesa <wanyamak884@gmail.com>
2025-03-06 02:10:17 +0300

    clone an object

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,57 +1,13 @@
-/**
- * Symbol for accessing private properties.
- */
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-/**
- * Class representing a Car.
- */
-class Car {
-  /**
-   * Create a Car.
-   * @param {String} brand - The brand of the car.
-   * @param {String} motor - The motor type of the car.
-   * @param {String} color - The color of the car.
-   */
+export default class Car {
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  /**
-   * Get the brand of the car.
-   * @returns {String} The brand of the car.
-   */
-  get brand() {
-    return this[_brand];
-  }
-
-  /**
-   * Get the motor type of the car.
-   * @returns {String} The motor type of the car.
-   */
-  get motor() {
-    return this[_motor];
-  }
-
-  /**
-   * Get the color of the car.
-   * @returns {String} The color of the car.
-   */
-  get color() {
-    return this[_color];
-  }
-
-  /**
-   * Clone the car and return a new instance with the same attributes.
-   * @returns {Car} A new Car instance with the same attributes.
-   */
   cloneCar() {
-    return new Car(this[_brand], this[_motor], this[_color]);
+    const obj = Object.create(this);
+    Object.assign(obj, { ...new Car() });
+    return obj;
   }
 }
-
-export default Car;
--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -1,4 +1,9 @@
-import listOfStudents, { StudentALX, ALXClass } from './9-hoisting.js';
+import listOfStudents from "./9-hoisting.js";
 
-console.log(listOfStudents[0] instanceof StudentALX);
-console.log(listOfStudents[0].alxClass instanceof ALXClass);
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
+
+console.log(listPrinted)
