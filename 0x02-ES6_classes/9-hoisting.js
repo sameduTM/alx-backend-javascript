d9968e291477b3b0792e734b65b70106cf3fd85f
Ken Wekesa <wanyamak884@gmail.com>
2025-03-05 15:01:28 +0300

    export the object after declaration and assignment

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -42,4 +42,7 @@ const student3 = new StudentALX('Albert', 'Clinton', class2019);
 const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
+
