a7c4ad16819dea812950624e193d0c7eb72afc27
Ken Wekesa <wanyamak884@gmail.com>
2025-03-06 03:21:25 +0300

    include ttype check for setter methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,9 @@
 export default class ALXCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError("Length must be a number");
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -22,14 +24,17 @@ export default class ALXCourse {
 
   // Setters
   set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     this._name = name;
   }
 
   set length(length) {
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
   }
 
   set students(students) {
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
     this._students = students;
   }
 }
