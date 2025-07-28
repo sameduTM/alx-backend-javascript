a01aead2d4726dddd752cf74428202d25eea5e99
Ken Wekesa <wanyamak884@gmail.com>
2025-07-28 17:40:58 +0300

    piped input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,26 @@
-console.log('Welcome to ALX, what is your name?');
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  const name = chunk.toString().trim();
-  if (name !== null) {
+if (!process.stdin.isTTY) {
+  // Non-interactive mode (piped input)
+  let data = '';
+  process.stdin.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  process.stdin.on('end', () => {
+    const name = data.toString().trim();
     console.log(`You name is: ${name}`);
-  }
-  process.exit(0);
-});
-
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+    console.log('This important software is now closing');
+  });
+} else {
+  // Interactive mode
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+      const name = chunk.toString().trim();
+      console.log(`You name is: ${name}`);
+      process.exit(0);
+    }
+  });
+}
