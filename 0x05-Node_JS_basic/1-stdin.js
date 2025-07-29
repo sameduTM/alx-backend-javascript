7727300a7c175e22f275cf3c8defe1f52251ca19
Ken Wekesa <wanyamak884@gmail.com>
2025-07-29 11:35:24 +0300

    use process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,20 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-if (!process.stdin.isTTY) {
-  // Non-interactive mode (piped input)
-  let data = '';
-  process.stdin.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  process.stdin.on('end', () => {
-    const name = data.toString().trim();
-    console.log(`You name is: ${name}`);
-    console.log('This important software is now closing');
-  });
-} else {
-  // Interactive mode
-  process.stdin.setEncoding('utf8');
+function takeInput () {
   process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
     if (chunk !== null) {
       const name = chunk.toString().trim();
-      console.log(`You name is: ${name}`);
-      process.exit(0);
+      console.log(`Your name is: ${name}`);
     }
+  });
+}
+
+if (process.stdin.isTTY) {
+  takeInput();
+} else {
+  takeInput();
+  setTimeout(() => {
+    process.stdout.write('This important software is now closing\n');
   });
 }
