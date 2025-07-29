c07140f1fb763d5efc05f2ea88f5b4f64ea3acad
Ken Wekesa <wanyamak884@gmail.com>
2025-07-29 20:01:00 +0300

    create small http server using node

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const { createServer } = require('node:http');
+
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader = ('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
+});
+
+app.listen(port);
