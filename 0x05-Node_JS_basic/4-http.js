11ac00b8569fd294a704829b3a3f8ac8e65ece5b
Ken Wekesa <wanyamak884@gmail.com>
2025-07-29 20:43:45 +0300

    small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,9 @@
-const createServer = require('http');
+const http = require('node:http');
 
 const port = 1245;
 
-const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader = ('Content-Type', 'text/plain');
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello ALX!');
 });
 
