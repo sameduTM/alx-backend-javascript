188450ed650e93fa2df64041521b31665ab1e53b
Ken Wekesa <wanyamak884@gmail.com>
2025-08-18 15:47:11 +0300

    use chai expect to test

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,24 @@
+/**
+ * Calculates the sum of two numbers after rounding them.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @param {string} type - Type of operation.
+ * @returns {number} The sum of the rounded numbers.
+ */
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+  return 'Error';
+}
+
+module.exports = calculateNumber;
