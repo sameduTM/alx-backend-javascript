986302c9c9395fe6dc231259072026a3b0172bc7
Ken Wekesa <wanyamak884@gmail.com>
2025-08-18 12:17:44 +0300

    strict equal

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('../0-calcul.js');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('should return the sum of rounded integers', () => {
