4a8c73212616d21cdda4ddd9b41f1e26f943eb89
Ken Wekesa <wanyamak884@gmail.com>
2025-08-19 21:37:41 +0300

    only SUM

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 const calculateNumber = require('./2-calcul');
 
 describe('chai - test the function calculateNumber', function () {
-    it('it should return the sum of a and b', function () {
+    it('SUM', function () {
         expect(calculateNumber('SUM', 1, 2)).to.equal(3);
         expect(calculateNumber('SUM', 1.7, 2)).to.equal(4);
         expect(calculateNumber('SUM', 1, 2.7)).to.equal(4);
