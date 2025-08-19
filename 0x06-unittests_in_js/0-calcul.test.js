0b6761815bfaae598943234e21c5db1cdd6c4515
Ken Wekesa <wanyamak884@gmail.com>
2025-08-19 17:41:22 +0300

    basic assertion test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,40 +1,27 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should return the sum of rounded integers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+// eslint-disable-next-line
+describe('tests calculateNumber', function () {
+  it('it should return the sum of a and b', function () {
+    assert.equal(calculateNumber(1, 2), 3);
   });
-
-  it('should handle rounding the first number up', () => {
-    assert.strictEqual(calculateNumber(1.7, 3), 5);
+  it('the first number is rounded up', function () {
+    assert.equal(calculateNumber(1.7, 2), 4);
   });
-
-  it('should handle rounding the second number up', () => {
-    assert.strictEqual(calculateNumber(1, 3.8), 5);
+  it('the second number is rounded up', function () {
+    assert.equal(calculateNumber(1, 2.7), 4);
   });
-
-  it('should handle rounding both numbers up', () => {
-    assert.strictEqual(calculateNumber(1.7, 3.8), 6);
+  it('both numbers are rounded up', function () {
+    assert.equal(calculateNumber(1.7, 2.7), 5);
   });
-
-  it('should handle rounding the first number down', () => {
-    assert.strictEqual(calculateNumber(1.2, 3), 4);
+  it('the first number is rounded down', function () {
+    assert.equal(calculateNumber(1.2, 2), 3);
   });
-
-  it('should handle rounding the second number down', () => {
-    assert.strictEqual(calculateNumber(1, 3.2), 4);
+  it('the second number is rounded down', function () {
+    assert.equal(calculateNumber(1, 2.2), 3);
   });
-
-  it('should handle rounding both numbers down', () => {
-    assert.strictEqual(calculateNumber(1.2, 3.2), 4);
-  });
-
-  it('should handle negative numbers', () => {
-    assert.strictEqual(calculateNumber(-1.2, -3.8), -5);
-  });
-
-  it('should handle rounding half numbers up', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+  it('both numbers rounded down', function () {
+    assert.equal(calculateNumber(1.2, 2,2), 3);
   });
 });
