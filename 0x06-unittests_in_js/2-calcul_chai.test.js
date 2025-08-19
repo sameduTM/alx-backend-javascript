a54b35522ea0fb31a60a874fca489010ec69c10a
Ken Wekesa <wanyamak884@gmail.com>
2025-08-19 21:31:52 +0300

    remove the describe nesting

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const calculateNumber = require('./2-calcul.js');
 
-describe('test the function calculateNumber', function () {
+describe('chai - test the function calculateNumber', function () {
     it('it should return the sum of a and b', function () {
         expect(calculateNumber('SUM', 1, 2)).to.equal(3);
         expect(calculateNumber('SUM', 1.7, 2)).to.equal(4);
@@ -13,67 +13,30 @@ describe('test the function calculateNumber', function () {
         expect(calculateNumber('SUM', 1, 2.2)).to.equal(3);
         expect(calculateNumber('SUM', 1.2, 2.2)).to.equal(3);
         expect(calculateNumber('SUM', 1, 2)).to.be.a('number');
+    });
 
-  });
-  describe('test the SUBTRACT operation', function () {
-    it('it should return the sum of a and b', function () {
+    it('it should return the difference of a and b', function () {
         expect(calculateNumber('SUBTRACT', 2, 1)).to.equal(1);
-      });
-      it('the first number is rounded up', function () {
         expect(calculateNumber('SUBTRACT', 2.7, 1)).to.equal(2);
-      });
-      it('the second number is rounded up', function () {
         expect(calculateNumber('SUBTRACT', 2, 1.7)).to.equal(0);
-      });
-      it('both numbers are rounded up', function () {
         expect(calculateNumber('SUBTRACT', 2.7, 1.7)).to.equal(1);
-      });
-      it('the first number is rounded down', function () {
         expect(calculateNumber('SUBTRACT', 2.2, 1)).to.equal(1);
-      });
-      it('the second number is rounded down', function () {
         expect(calculateNumber('SUBTRACT', 2, 2.2)).to.equal(0);
-      });
-      it('both numbers rounded down', function () {
         expect(calculateNumber('SUBTRACT', 1.2, 2.2)).to.equal(-1);
-      });
-      it('both numbers are negative', function() {
         expect(calculateNumber('SUBTRACT', -1, -2)).to.equal(1);
-      });
-      it('the first number is negative', function() {
         expect(calculateNumber('SUBTRACT', -2, 1)).to.equal(-3);
-      });
-      it('the second number is negative', function () {
         expect(calculateNumber('SUBTRACT', 2, -1)).to.equal(3);
-      });
-       it('return type should be number', function () {
         expect(calculateNumber('SUBTRACT', 1, 2)).to.be.a('number');
-      })
-  });
-  describe('test the DIVIDE operation of the function', function () {
-    it('it should return the sum of a and b', function () {
+    });
+
+    it('it should return the division of a and b', function () {
         expect(calculateNumber('DIVIDE', 1, 2)).to.equal(0.5);
-      });
-      it('the first number is negative', function () {
         expect(calculateNumber('DIVIDE', -4, 2)).to.equal(-2);
-      });
-      it('the second number is negative', function () {
         expect(calculateNumber('DIVIDE', 4, -2)).to.equal(-2);
-      });
-      it('both numbers are negative', function () {
         expect(calculateNumber('DIVIDE', -4, -2)).to.equal(2);
-      });
-      it('the second number is 0', function () {
         expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
-      });
-      it('second number is rounded down to zero', function () {
         expect(calculateNumber('DIVIDE', 2, 0.3)).to.equal('Error');
-      });
-       it('return type should be number', function () {
         expect(calculateNumber('DIVIDE', 1, 2)).to.be.a('number');
-      });
-      it('return type should be string if b == 0', function () {
         expect(calculateNumber('DIVIDE', 1, 0)).to.be.a('string');
-      });
-  });
+    });
 });
