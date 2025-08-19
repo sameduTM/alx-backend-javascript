a2581859c327cc6c157c6e7bed04410f0c563d7a
Ken Wekesa <wanyamak884@gmail.com>
2025-08-19 21:35:58 +0300

    only include test case for type=SUM

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const chai = require('chai');
 const expect = chai.expect;
-const calculateNumber = require('./2-calcul.js');
+const calculateNumber = require('./2-calcul');
 
 describe('chai - test the function calculateNumber', function () {
     it('it should return the sum of a and b', function () {
@@ -13,30 +13,5 @@ describe('chai - test the function calculateNumber', function () {
         expect(calculateNumber('SUM', 1, 2.2)).to.equal(3);
         expect(calculateNumber('SUM', 1.2, 2.2)).to.equal(3);
         expect(calculateNumber('SUM', 1, 2)).to.be.a('number');
-    });
-
-    it('it should return the difference of a and b', function () {
-        expect(calculateNumber('SUBTRACT', 2, 1)).to.equal(1);
-        expect(calculateNumber('SUBTRACT', 2.7, 1)).to.equal(2);
-        expect(calculateNumber('SUBTRACT', 2, 1.7)).to.equal(0);
-        expect(calculateNumber('SUBTRACT', 2.7, 1.7)).to.equal(1);
-        expect(calculateNumber('SUBTRACT', 2.2, 1)).to.equal(1);
-        expect(calculateNumber('SUBTRACT', 2, 2.2)).to.equal(0);
-        expect(calculateNumber('SUBTRACT', 1.2, 2.2)).to.equal(-1);
-        expect(calculateNumber('SUBTRACT', -1, -2)).to.equal(1);
-        expect(calculateNumber('SUBTRACT', -2, 1)).to.equal(-3);
-        expect(calculateNumber('SUBTRACT', 2, -1)).to.equal(3);
-        expect(calculateNumber('SUBTRACT', 1, 2)).to.be.a('number');
-    });
-
-    it('it should return the division of a and b', function () {
-        expect(calculateNumber('DIVIDE', 1, 2)).to.equal(0.5);
-        expect(calculateNumber('DIVIDE', -4, 2)).to.equal(-2);
-        expect(calculateNumber('DIVIDE', 4, -2)).to.equal(-2);
-        expect(calculateNumber('DIVIDE', -4, -2)).to.equal(2);
-        expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
-        expect(calculateNumber('DIVIDE', 2, 0.3)).to.equal('Error');
-        expect(calculateNumber('DIVIDE', 1, 2)).to.be.a('number');
-        expect(calculateNumber('DIVIDE', 1, 0)).to.be.a('string');
     });
 });
