21a7c2de021464541032069fca7b8b392dee19b1
Ken Wekesa <wanyamak884@gmail.com>
2025-08-20 21:31:49 +0300

    hooks chai

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+const sendPaymentRequestToApi = require('./4-payment');
+const sinon = require('sinon');
+const { expect, assert } = require('chai');
+
+describe('calls sendPaymentRequestToApi with 100, and 200', function () {
+  let spy;
+  beforeEach(function () {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    spy.restore();
+  });
+
+  it('calls sendPaymentRequestToApi with 100, and 200', function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith("The total is: 120")).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+  it('calls sendPaymentRequestToApi with 10, and 10', function () {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.calledWith("The total is: 20")).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+});
