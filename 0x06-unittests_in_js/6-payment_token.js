1cb8ae7b8fd70953c0f6ac23faf6c6403d3b3149
Ken Wekesa <wanyamak884@gmail.com>
2025-08-21 15:55:57 +0300

    add catch to handle error

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 module.exports = function getPaymentTokenFromAPI(success) {
   if (success === true) {
-    return new Promise((resolve) => resolve(
+    return new Promise((resolve, reject) => resolve(
       { data: 'Successful response from the API' },
     ));
   }
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,9 @@ const { expect, assert } = require('chai');
 
 describe('getPaymentTokenFromAPI', function () {
   it('test the result of getPaymentTokenFromAPI(true)', function(done) {
-    getPaymentTokenFromAPI(true).then((response) => {
-      console.log(response);
-    }).then(done);
-  })
-})
+    getPaymentTokenFromAPI(true).then(response => {
+      assert.equal(response.data, 'Successful response from the API');
+      done();
+    }).catch((err) => done(err));
+  });
+});
